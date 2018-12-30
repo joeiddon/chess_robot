785fc659c6da2859dfc9168c3f7ce3fc2ab136f5
joeiddon <joedominiciddon@gmail.com>
2018-12-30 19:49:15 +0000

    better communication with Arduino (error codes handled and a blocking function to wait till arrive at desired postition)

--- arm.py
+++ arm.py
@@ -1,7 +1,10 @@
 import serial, os.path
 
+#TODO: COPY IN THE FULL PROTOCOL FROM THE SKETCH
+
 """
 ==Joe's Serial Protocol==
+  Runs at a baud rate of 115200 pulses per second.
   First byte is message type; last byte is a full terminater byte (0xff).
   The number of enclosed bytes (NEBs) is dependent on the message type.
   First byte  | Message (type)  | NEBs | Enclosed bytes meaning
@@ -10,8 +13,15 @@ import serial, os.path
   2           | set grabber     | 1    | (0/1 => grab off/grab on
   3           | move position   | 6    | (x: [0¬340, y: [100¬410], z: [0¬200])
                                           ^          ^             ^
-                         these uint16s are sent *little-endian* (see INT_FROM_BYTES)
-  Will return a standard success code: 0/1 indicating success/failure, respectively.
+       these uint16s (actually casted to signed) are sent *little-endian* (see INT_FROM_BYTES)
+
+  Arduino will reply with a one byte code after every message.
+  Value       | Meaning
+  0           | Task completed as expected
+  1           | x_pos has reached x_target - useful for timings
+  2           | Invalid request structure (wrong NEBs)
+  3           | Task failed due to invalid input ranges
+  4           | Unkown request type
 """
 
 LIMITS = {'x': (0, 340),
@@ -24,44 +34,51 @@ class Arm():
     C++ code on.'''
     def __init__(self, port):
         '''Initialises an arm instance.
-            - port <= serial port string
-        '''
+            - port <= serial port string'''
         self.port = port
         self._connect_to_arduino()
     def home(self):
         '''Moves the arm to its home position which is
         defined on the Arduino's side.'''
-        self.serial.write([0, 0xff])
+        self._write_and_check_success([0, 0xff])
     def set_motors(self, state):
         '''Enables or disables the stepper and servo motors on the arm.
             - state <= boolean: True=enable; False=disable'''
-        self.serial.write([1, state, 0xff])
+        self._write_and_check_success([1, state, 0xff])
     def set_grabber(self, state):
         '''Sets the state of the grabber on the arm's effector.
             - state <= boolean: True=grab on; False=grab off'''
-        self.serial.write([2, state, 0xff])
+        self._write_and_check_success([2, state, 0xff])
     def move_to(self, x, y, z):
         '''Moves the arm to the specified coordinate; given in millimeters.'''
         if not (LIMITS['x'][0] <= x <= LIMITS['x'][1] and \
                 LIMITS['y'][0] <= y <= LIMITS['y'][1] and \
                 LIMITS['z'][0] <= z <= LIMITS['z'][1]):
             raise ValueError(f'Position [{x},{y},{z}] out of limits.')
-        self.serial.write([3,
-                           *self._bytes_from_int(x),
-                           *self._bytes_from_int(y),
-                           *self._bytes_from_int(z),
-                           0xff])
-    def _send_joes_protocol(self, message):
-        self.serial.read(self.serial.is_waiting)
+        self._write_and_check_success([3,
+                                       *self._bytes_from_int(x),
+                                       *self._bytes_from_int(y),
+                                       *self._bytes_from_int(z),
+                                       0xff])
+    def block_till_reach_target(self):
+        '''Waits till x_pos reaches next x_target. Blocking.'''
+        self._flush_serial()
+        r = self.serial.read()[0]
+        if r != 1:
+            raise Exception(f'Arduino return code != 1: {r}')
+    def _flush_serial(self):
+        if self.serial.in_waiting:
+            print(f'WARNING: Unexpected message from Arduino: {list(self.serial.read(self.serial.in_waiting))}')
+    def _write_and_check_success(self, message):
+        self._flush_serial()
         self.serial.write(message)
-        if self.serial.is_waiting != 1:
-            raise Exception(f'Arduino replied with more than one byte when sending: {message}.')
         if self.serial.read()[0] != 0:
-            raise Exception(f'Arduino status code non-zero wwhen sending: {message}.')
+            raise Exception(f'Arduino code non-zero when sending: {message}.'+
+                            f'It replied with: {list(self.serial.read(self.serial.in_waiting))}')
     def _connect_to_arduino(self):
         try:
-            self.serial = serial.Serial(self.port)
+            self.serial = serial.Serial(self.port, 115200);
         except serial.serialutil.SerialException:
-            raise Exception(f'Could not open [{port}].')
+            raise Exception(f'Could not open [{self.port}].')
     def _bytes_from_int(self, i):
         return (i >> 8, i & 0xff)
